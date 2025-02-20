391c4e46678e7135c88bf59b994b8e3ebf44dfdd
Guilherme Silva <sgsouza.173@gmail.com>
2025-02-20 14:17:45 -0300

    feat: Criacao das funcoes CRUD nas classes de Service e Controller, com tratamento de ResponseEntity

--- src/main/java/com/movieflix/service/CategoryService.java
+++ src/main/java/com/movieflix/service/CategoryService.java
@@ -2,10 +2,10 @@ package com.movieflix.service;
 
 import com.movieflix.entity.Category;
 import com.movieflix.repository.CategoryRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -17,5 +17,17 @@ public class CategoryService {
 
     public List<Category> findAll() {
         return repository.findAll();
+    }
+
+    public Category saveCategory(Category category) {
+        return repository.save(category);
+    }
+
+    public Optional<Category> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public void deleteCategory(Long id) {
+        repository.deleteById(id);
     }
 }
