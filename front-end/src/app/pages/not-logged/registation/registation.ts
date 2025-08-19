47aaee03a1c682dadd322915e467bac81608e8d9
GuilhermeD9 <sgsouza.173@gmail.com>
2025-08-19 14:11:00 -0300

    feat: New angular components

---
+++ front-end/src/app/pages/not-logged/registation/registation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registation',
+  imports: [],
+  templateUrl: './registation.html',
+  styleUrl: './registation.css'
+})
+export class Registation {
+
+}
